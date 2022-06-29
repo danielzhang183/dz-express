817071f4c034df207c8047e14fd74d5cd9b4adc4
danielzhang183 <danielzhang182@gmail.com>
2022-06-29 15:36:05 +0800

    chore: add db.json & complete list & detail api

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const fs = require('fs')
+const { getDb } = require('./db')
 
 const app = express();
 
-app.get('/todos', (req, res) => {
-  res.send('get /todos');
+app.get('/todos', async (req, res) => {
+  fs.readFile('./db.json', 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message
+      })
+    }
+    const db = JSON.parse(data)
+    res.status(200).json(db.todos)
+  })
 });
 
 app.get('/todos/:id', (req, res) => {
-  res.send(`get /todos/${req.params.id}`);
+  fs.readFile('./db.json', 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message
+      })
+    }
+    const db = JSON.parse(data)
+    const todo = db.todos.find(({ id }) => id === Number(req.params.id))
+    todo
+      ? res.status(200).json(todo)
+      : res.status(404).end()
+  })
 });
 
 app.post('/todos', (req, res) => {
