309219654896505f2609daae77e2159f8f41dfc4
danielzhang183 <danielzhang182@gmail.com>
2022-06-30 01:33:04 +0800

    chore: global handle 404

--- app.js
+++ app.js
@@ -9,8 +9,15 @@ app.use((req, res, next) => {
   next()
 })
 
+// add router
 app.use('/todos', router)
 
+// handle 404
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found.')
+})
+
+// handle error
 app.use((err, req, res, next) => {
   res.status(500).json({
     error: err.message
